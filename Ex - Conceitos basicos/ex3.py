773b9861c165554142791453cfde2d25f7bf6fd8
Lívia <liviapnd@gmail.com>
2024-01-19 19:05:03 -0300

    exercício 03

---
+++ Ex - Conceitos basicos/ex3.py
@@ -0,0 +1,6 @@
+print("Por favor, digite a quantidade de quilômetros.")
+km = float(input())
+
+print(f"{km} km em metros equivale a: {km*1000} m")
+print(f"{km} km em centímetros equivale a: {km*100000} cm")
+print(f"{km} km em milímetros equivale a: {km*1000000} mm")

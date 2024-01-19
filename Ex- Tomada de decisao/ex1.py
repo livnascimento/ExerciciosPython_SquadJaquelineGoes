375a485f0df5af0e0d183ebf5b671f1318971ca3
Lívia <liviapnd@gmail.com>
2024-01-19 20:13:11 -0300

    exercício 01

--- Ex- Tomada de decisao/ex1.py
+++ Ex- Tomada de decisao/ex1.py
@@ -1,1 +1,6 @@
-# arquivo de teste para ver se o comit está funcionando
+print("Por favor, insira dois números.")
+
+numero1 = int(input())
+numero2 = int(input())
+
+print(numero1 if numero1 > numero2 else numero2)

326ffb21a38abed174f93523836bc761ac61ee0f
Lívia <liviapnd@gmail.com>
2024-01-19 19:43:36 -0300

    exercício 06

---
+++ Ex - Conceitos basicos/ex6.py
@@ -0,0 +1,6 @@
+distancia = float(input("Digite a distância a ser percorrida.\n"))
+
+print("Para esta viagem, você gastaria:")
+print(f"{distancia/600} horas de avião,")
+print(f"{distancia/100} horas de carro e")
+print(f"{distancia/80} horas de ônibus")

a8fd3ed4e479b697d06480a73f90e96c0cbb02d9
Lívia <liviapnd@gmail.com>
2024-01-19 18:42:00 -0300

    exercício 01

--- Ex - Conceitos basicos/ex1.py
+++ Ex - Conceitos basicos/ex1.py
@@ -1,1 +1,9 @@
-# arquivo de teste para ver se o comit está funcionando
+print("Por favor, digite dois números inteiros.")
+
+numero1 = int(input())
+numero2 = int(input())
+
+print(f"{numero1} + {numero2} = {numero1 + numero2}")
+print(f"{numero1} - {numero2} = {numero1 - numero2}")
+print(f"{numero1} / {numero2} = {numero1 / numero2}")
+print(f"{numero1} * {numero2} = {numero1 * numero2}")

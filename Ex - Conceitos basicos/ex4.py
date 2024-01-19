ed16ea8cc502fb6601e9aeb0d199e14d57f7ba5a
Lívia <liviapnd@gmail.com>
2024-01-19 19:11:51 -0300

    exercício 04

---
+++ Ex - Conceitos basicos/ex4.py
@@ -0,0 +1,5 @@
+litrosCombustivel = float(input("Por favor, insira a quantidade de combustível gasta (em litros).\n"))
+
+distanciaPercorrida = float(input("Insira a distância percorrida (em km).\n"))
+
+print(f"\nO consumo médio foi de {distanciaPercorrida/litrosCombustivel} km/l")

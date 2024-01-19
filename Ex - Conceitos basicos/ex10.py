2190089700924054393c3df36b99e2e9a246d7a1
Lívia <liviapnd@gmail.com>
2024-01-19 20:08:43 -0300

    exercício 10

---
+++ Ex - Conceitos basicos/ex10.py
@@ -0,0 +1,6 @@
+aluno = input("Insira seu nome.\n")
+universidade = input("Insira a sua instituição de ensino.\n")
+curso = input("Qual é o nome do curso?\n")
+semestre = input("Qual semestre você está cursando atualmente?\n")
+
+print(f"Estudante {aluno} está no {semestre}º semestre do curso de {curso} na instituição {universidade}.")

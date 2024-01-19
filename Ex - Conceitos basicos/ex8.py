fcfd9745b8b8dcc0cb24a7dfbe33b8baea2dc240
Lívia <liviapnd@gmail.com>
2024-01-19 19:55:06 -0300

    exercício 08

---
+++ Ex - Conceitos basicos/ex8.py
@@ -0,0 +1,4 @@
+valorPorHora = float(input("Qual é o valor da hora?\n"))
+horasPorMes = float(input("Insira a quantidade de horas trabalhadas por mês.\n"))
+
+print(f"O salário mensal será de R$ {valorPorHora * horasPorMes}")

c5dc678845702c8f984097446c2400248b7c235b
Lívia <liviapnd@gmail.com>
2024-01-19 20:16:09 -0300

    exercício 02

---
+++ Ex- Tomada de decisao/ex2.py
@@ -0,0 +1,10 @@
+turno = input("Em qual turno você estuda?\n")
+
+if ( turno == 'M' ):
+    print("Bom dia!")
+elif ( turno == 'V' ):
+    print("Boa tarde!")
+elif ( turno == 'N' ):
+    print("Boa noite!")
+else:
+    print("Valor inválido!")

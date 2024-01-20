f5441dc8440e9ecf30a11c6ddc512f81199944f5
Lívia <liviapnd@gmail.com>
2024-01-20 09:37:41 -0300

    exercício 01

--- Ex - Listas tuplas e dicio/ex1.py
+++ Ex - Listas tuplas e dicio/ex1.py
@@ -1,1 +1,24 @@
-# arquivo de teste para ver se o comit está funcionando
+perguntas = [
+    "Telefonou para a vítima?",
+    "Esteve no local do crime?",
+    "Mora perto da vítima?",
+    "Devia para a vítima?",
+    "Já trabalhou com a vítima?"
+]
+
+afirmativo = 0
+
+for pergunta in perguntas:
+    print(pergunta)
+    resposta = input()
+    if (resposta == 's'):
+        afirmativo += 1
+
+if (afirmativo < 2):
+    print("Inocente.")
+elif (afirmativo == 2):
+    print("Suspeito.")
+elif (afirmativo <= 4):
+    print("Cúmplice.")
+else:
+    print("Assassino.")

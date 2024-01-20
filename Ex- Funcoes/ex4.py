681277cf74a5e36edf79b930f6d18a8b7500e973
Lívia <liviapnd@gmail.com>
2024-01-20 12:17:06 -0300

    exercício 04

---
+++ Ex- Funcoes/ex4.py
@@ -0,0 +1,10 @@
+valor = int(input("Digite o saldo atual.\n"))
+
+print(f"Com {valor} reais você consegue comprar:")
+print(f"{valor / 4.91:.2f} dólares americanos")
+print(f"{valor / 0.02:.2f} pesos argentinos")
+print(f"{valor / 3.18:.2f} dólares australianos")
+print(f"{valor / 3.64:.2f} dólares canadenses")
+print(f"{valor / 0.42:.2f} francos suiços")
+print(f"{valor / 5.36:.2f} euros")
+print(f"{valor / 6.21:.2f} libras esterlinas")

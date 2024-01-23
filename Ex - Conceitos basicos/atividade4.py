5e44e9109736d2451d6a6d8b51182bac3a0033a4
devnayarasilva <nay.aguiar.s@gmail.com>
2024-01-22 22:40:47 -0300

    atividade4

---
+++ Ex - Conceitos basicos/atividade4.py
@@ -0,0 +1,13 @@
+
+'''"Receba do usuário a quantidade de litros de combustível consumidos e a distância percorrida. 
+Calcule e imprima o consumo médio em km/l."'''
+
+
+litros_consumidos = float(input("Digite a quantidade de litros de combustível consumidos: "))
+
+distancia_percorrida = float(input("Digite a distância percorrida em quilômetros: "))
+
+# valor final
+consumo_medio = distancia_percorrida / litros_consumidos
+
+print(f"O consumo médio é de {consumo_medio:.2f} km/l")
